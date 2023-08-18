d7b063b1d165fd2ed212bce2120646a68755a747
sandenson <lgabrielsandes@gmail.com>
2023-08-18 17:12:56 -0300

    feat: Create Prisma module.

--- apps/nest/src/app/app.module.ts
+++ apps/nest/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from '../common/app/env-validation-schema.const';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
     validationSchema: envValidationSchema,
     validationOptions: {
       allowUnknown: false,
     },
     expandVariables: true,
-  })],
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
